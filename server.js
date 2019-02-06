af5ea45953a864e02cdb1ea7d4cd4a2c6898ad34
Sachin.Khapali <sachin.khapali@Sachin-khapali.local>
2019-02-06 23:30:47 +0530

    Myfiles

---
+++ server.js
@@ -0,0 +1,31 @@
+﻿require('rootpath')();
+const express = require('express');
+const helmet = require('helmet');
+//const https = require("https");
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
+
+app.use(helmet({ dnsPrefetchControl: { allow: true }}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors({'origin': 'http://localhost:4200'}))
+  
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+//https.createServer(app).listen(port);
+//console.log('Server listening on port ' + port);
+const server = app.listen(port, function () {
+     console.log('Server listening on port ' + port);
+});
